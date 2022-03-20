3261bbad014f21b9c93b89f8afdb81c0ac43cbf5
Timur Malik <timur-malik@mail.ru>
2022-03-20 20:54:42 +0300

    initial commit

---
+++ command_service/src/main/java/teamcoffee/command/CommandServiceMain.java
@@ -0,0 +1,13 @@
+package teamcoffee.command;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class CommandServiceMain {
+    public static void main(String[] args) {
+        SpringApplication.run(CommandServiceMain.class, args);
+    }
+}
