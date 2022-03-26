7ab826021e97f236b79489a8ea47cae750fc8d3f
Timur Malik <timur-malik@mail.ru>
2022-03-26 20:45:44 +0300

    add new method for json

--- router_service/src/main/java/teamcoffee/routerservice/reports/ReportToAccountantService.java
+++ router_service/src/main/java/teamcoffee/routerservice/reports/ReportToAccountantService.java
@@ -1,4 +1,40 @@
 package teamcoffee.routerservice.reports;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.client.RestTemplate;
+
 public class ReportToAccountantService {
+
+
+    private RestTemplate restTemplate = new RestTemplate();
+
+    public static void main(String[] args) {
+        TaskDTO taskDTO = new TaskDTO(0, "GG","PP");
+        ReportToAccountantService reportToAccountantService = new ReportToAccountantService();
+        reportToAccountantService.deleteTaskDTO(5);
+
+    }
+
+    public void getAllTasks() {
+        String url = "http://localhost:8191/tasks/1";
+        TaskDTO json = restTemplate.getForObject(url,TaskDTO.class);
+        System.out.println(json);
+    }
+
+    public void postNewTask(TaskDTO taskDTO) {
+        String url = "http://localhost:8191/tasks";
+        String json = restTemplate.postForObject(url,taskDTO,String.class);
+        System.out.println(json);
+    }
+
+    public void putTaskDTO(int id, TaskDTO taskDTO){
+        String url = "http://localhost:8191/tasks/" + id;
+        restTemplate.put(url,taskDTO);
+    }
+
+    public void deleteTaskDTO(int id){
+        String url = "http://localhost:8191/tasks/" + id;
+        restTemplate.delete(url);
+    }
+
 }
