f88822590459a8daf8b3f68c1dfb82943d4b1d26
Asus <dgndfgh>
2022-03-26 13:50:12 +0300

    Add DTO classes

--- accountant_service/src/main/java/teamcoffee/accountant/dto/ReportDTO.java
+++ accountant_service/src/main/java/teamcoffee/accountant/dto/ReportDTO.java
@@ -1,4 +1,88 @@
 package teamcoffee.accountant.dto;
 
+import java.util.List;
+import java.util.Objects;
+
 public class ReportDTO {
+
+    private int id;
+    private long date;
+    private long fullTime;
+    private long userChatId;
+    private List<TrackingDTO> dayTrackings;
+
+    public ReportDTO() {
+    }
+
+    public ReportDTO(int id, long date, long fullTime, long userChatId, List<TrackingDTO> dayTrackings) {
+        this.id = id;
+        this.date = date;
+        this.fullTime = fullTime;
+        this.userChatId = userChatId;
+        this.dayTrackings = dayTrackings;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public long getDate() {
+        return date;
+    }
+
+    public void setDate(long date) {
+        this.date = date;
+    }
+
+    public long getFullTime() {
+        return fullTime;
+    }
+
+    public void setFullTime(long fullTime) {
+        this.fullTime = fullTime;
+    }
+
+    public long getUserChatId() {
+        return userChatId;
+    }
+
+    public void setUserChatId(long userChatId) {
+        this.userChatId = userChatId;
+    }
+
+    public List<TrackingDTO> getDayTrackings() {
+        return dayTrackings;
+    }
+
+    public void setDayTrackings(List<TrackingDTO> dayTrackings) {
+        this.dayTrackings = dayTrackings;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ReportDTO reportDTO = (ReportDTO) o;
+        return id == reportDTO.id && date == reportDTO.date && fullTime == reportDTO.fullTime && userChatId == reportDTO.userChatId && Objects.equals(dayTrackings, reportDTO.dayTrackings);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, date, fullTime, userChatId, dayTrackings);
+    }
+
+    @Override
+    public String toString() {
+        return "ReportDTO{" +
+                "id=" + id +
+                ", date=" + date +
+                ", fullTime=" + fullTime +
+                ", userChatId=" + userChatId +
+                ", dayTrackings=" + dayTrackings +
+                '}';
+    }
 }
--- accountant_service/src/main/java/teamcoffee/accountant/dto/TaskDTO.java
+++ accountant_service/src/main/java/teamcoffee/accountant/dto/TaskDTO.java
@@ -1,8 +1,65 @@
 package teamcoffee.accountant.dto;
 
+import java.util.Objects;
+
 public class TaskDTO {
 
     private int id;
     private String name;
     private String note;
+
+    public TaskDTO() {
+    }
+
+    public TaskDTO(int id, String name, String note) {
+        this.id = id;
+        this.name = name;
+        this.note = note;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TaskDTO taskDTO = (TaskDTO) o;
+        return id == taskDTO.id && Objects.equals(name, taskDTO.name) && Objects.equals(note, taskDTO.note);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, note);
+    }
+
+    @Override
+    public String toString() {
+        return "TaskDTO{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", note='" + note + '\'' +
+                '}';
+    }
 }
